7d216e76b376d00ef061a6048ea247da252095a4
nayankadamm <nayankadam391@gmail.com>
2024-08-20 14:30:33 +0530

    second push

--- client.c
+++ client.c
@@ -1,149 +1,310 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <unistd.h>
+
 #include <arpa/inet.h>
+
 #include <fcntl.h>
- 
+
+
+
 #define PORT 8080
+
 #define BUF_SIZE 1024
+
 #define END_OF_TRANSFER "END_OF_TRANSFER"
- 
+
+
+
 void upload_file(int sock);
+
 void download_file(int sock);
+
 void list_files(int sock);
+
 void delete_file(int sock);
- 
+
+
+
 int main() {
+
     int sock = 0;
+
     struct sockaddr_in serv_addr;
+
     char command[BUF_SIZE] = {0};
- 
+
+
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
         perror("Socket creation failed");
+
         exit(EXIT_FAILURE);
+
     }
- 
+
+
+
     serv_addr.sin_family = AF_INET;
+
     serv_addr.sin_port = htons(PORT);
- 
+
+
+
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+
         perror("Invalid address/ Address not supported");
+
         exit(EXIT_FAILURE);
+
     }
- 
+
+
+
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+
         perror("Connection Failed");
+
         exit(EXIT_FAILURE);
+
     }
- 
+
+
+
     while (1) {
+
         printf("Enter command (UPLOAD/DOWNLOAD/LIST/DELETE/QUIT): ");
+
         fgets(command, BUF_SIZE, stdin);
+
         command[strcspn(command, "\n")] = 0; // Remove newline character
- 
+
+
+
         send(sock, command, strlen(command), 0);
- 
+
+
+
         if (strncmp(command, "UPLOAD", 6) == 0) {
+
             upload_file(sock);
+
         } else if (strncmp(command, "DOWNLOAD", 8) == 0) {
+
             download_file(sock);
+
         } else if (strncmp(command, "LIST", 4) == 0) {
+
             list_files(sock);
+
         } else if (strncmp(command, "DELETE", 6) == 0) {
+
             delete_file(sock);
+
         } else if (strncmp(command, "QUIT", 4) == 0) {
+
             break;
+
         } else {
+
             printf("Invalid command.\n");
+
         }
- 
+
+
+
         memset(command, 0, BUF_SIZE); // Clear command buffer
+
     }
- 
+
+
+
     close(sock);
+
     return 0;
+
 }
- 
+
+
+
 void upload_file(int sock) {
+
     char buffer[BUF_SIZE] = {0};
+
     char filename[BUF_SIZE] = {0};
+
     int file_fd, bytes_read;
- 
+
+
+
     printf("Enter filename to upload: ");
+
     fgets(filename, BUF_SIZE, stdin);
+
     filename[strcspn(filename, "\n")] = 0;
- 
-    file_fd = open(filename, O_RDONLY |);
+
+
+
+    file_fd = open(filename, O_RDONLY);
+
     if (file_fd < 0) {
+
         perror("File open failed");
+
         return;
+
     }
- 
+
+
+
     send(sock, filename, strlen(filename), 0);
-    read(sock, buffer, BUF_SIZE); // Wait for server acknowledgment
- 
+
+    read(sock, buffer, BUF_SIZE); 
+
+
+
     while ((bytes_read = read(file_fd, buffer, BUF_SIZE)) > 0) {
+
         send(sock, buffer, bytes_read, 0);
+
     }
- 
-    // Indicate end of file transfer
+
+    
+
+    // Send end of transfer signal
+
     send(sock, END_OF_TRANSFER, strlen(END_OF_TRANSFER), 0);
- 
+
+
+
     close(file_fd);
-    printf("File %s uploaded successfully.\n", filename);
+
+    printf("File uploaded successfully.\n");
+
+
+
+    // Open the file
+
+    char command[BUF_SIZE];
+
+    snprintf(command, BUF_SIZE, "xdg-open %s", filename); 
+
+    system(command);
+
 }
- 
+
+
+
+
+
 void download_file(int sock) {
+
     char buffer[BUF_SIZE] = {0};
+
     char filename[BUF_SIZE] = {0};
+
     int file_fd, bytes_read;
- 
+
+
+
     printf("Enter filename to download: ");
+
     fgets(filename, BUF_SIZE, stdin);
+
     filename[strcspn(filename, "\n")] = 0;
- 
+
+
+
     send(sock, filename, strlen(filename), 0);
-    read(sock, buffer, BUF_SIZE); // Wait for server acknowledgment
- 
+
+    read(sock, buffer, BUF_SIZE); 
+
+
+
     file_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+
     if (file_fd < 0) {
+
         perror("File open failed");
+
         return;
+
     }
- 
+
+
+
     while ((bytes_read = read(sock, buffer, BUF_SIZE)) > 0) {
+
         if (strncmp(buffer, END_OF_TRANSFER, strlen(END_OF_TRANSFER)) == 0) {
+
             break;
+
         }
+
         write(file_fd, buffer, bytes_read);
+
     }
- 
+
+
+
     close(file_fd);
-    printf("File %s downloaded successfully.\n", filename);
+
+    printf("File downloaded successfully.\n");
+
+
+
+    // Open the file
+
+    char command[BUF_SIZE];
+
+    snprintf(command, BUF_SIZE, "xdg-open %s", filename); 
+
+    system(command);
+
 }
- 
+
+
+
+
+
 void list_files(int sock) {
+
     char buffer[BUF_SIZE] = {0};
-    int bytes_read;
- 
-    while ((bytes_read = read(sock, buffer, BUF_SIZE)) > 0) {
-        printf("%s", buffer);
-        if (bytes_read < BUF_SIZE) {
-            break;
-        }
-    }
+
+
+
+    read(sock, buffer, BUF_SIZE);
+
+    printf("Files:\n%s", buffer);
+
 }
- 
+
+
+
 void delete_file(int sock) {
+
     char filename[BUF_SIZE] = {0};
-    char buffer[BUF_SIZE] = {0};
- 
+
+
+
     printf("Enter filename to delete: ");
+
     fgets(filename, BUF_SIZE, stdin);
+
     filename[strcspn(filename, "\n")] = 0;
- 
+
+
+
     send(sock, filename, strlen(filename), 0);
-    read(sock, buffer, BUF_SIZE); // Wait for server acknowledgment
-    printf("Delete request for file %s sent.\n", filename);
+
+    read(sock, filename, BUF_SIZE); 
+
+    printf("File deletion request sent.\n");
+
 }
+
--- server.c
+++ server.c
@@ -1,169 +1,404 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <unistd.h>
+
 #include <arpa/inet.h>
+
 #include <fcntl.h>
+
 #include <dirent.h>
- 
+
+#include <errno.h>
+
+#include <sys/stat.h>
+
+#include <sys/types.h>
+
+
+
 #define PORT 8080
+
 #define BUF_SIZE 1024
+
 #define END_OF_TRANSFER "END_OF_TRANSFER"
- 
+
+#define ROOT_DIR "root" // Root directory on the server
+
+
+
 void handle_upload(int new_socket);
+
 void handle_download(int new_socket);
+
 void handle_list(int new_socket);
+
 void handle_delete(int new_socket);
- 
+
+
+
 int main() {
+
     int server_fd, new_socket;
+
     struct sockaddr_in address;
+
     int addrlen = sizeof(address);
+
     char buffer[BUF_SIZE] = {0};
- 
+
+
+
+    // Create root directory if it doesn't exist
+
+    if (mkdir(ROOT_DIR, 0777) == -1 && errno != EEXIST) {
+
+        perror("mkdir failed");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+
+
     // Create socket
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+
         perror("Socket failed");
+
         exit(EXIT_FAILURE);
+
     }
- 
+
+
+
     // Bind socket
+
     address.sin_family = AF_INET;
+
     address.sin_addr.s_addr = INADDR_ANY;
+
     address.sin_port = htons(PORT);
- 
+
+
+
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+
         perror("Bind failed");
+
         close(server_fd);
+
         exit(EXIT_FAILURE);
+
     }
- 
+
+
+
     // Listen for incoming connections
+
     if (listen(server_fd, 3) < 0) {
+
         perror("Listen failed");
+
         close(server_fd);
+
         exit(EXIT_FAILURE);
+
     }
- 
+
+
+
     printf("Server is listening on port %d\n", PORT);
- 
+
+
+
     while (1) {
+
         // Accept incoming connection
+
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+
             perror("Accept failed");
-            close(server_fd);
-            exit(EXIT_FAILURE);
+
+            continue;
+
         }
- 
-        // Receive command
-        memset(buffer, 0, BUF_SIZE);
-        read(new_socket, buffer, BUF_SIZE);
-        printf("Received command: %s\n", buffer);
- 
-        if (strncmp(buffer, "UPLOAD", 6) == 0) {
-            send(new_socket, "ACK", 3, 0);  // Send acknowledgment
-            handle_upload(new_socket);
-        } else if (strncmp(buffer, "DOWNLOAD", 8) == 0) {
-            send(new_socket, "ACK", 3, 0);  // Send acknowledgment
-            handle_download(new_socket);
-        } else if (strncmp(buffer, "LIST", 4) == 0) {
-            handle_list(new_socket);
-        } else if (strncmp(buffer, "DELETE", 6) == 0) {
-            send(new_socket, "ACK", 3, 0);  // Send acknowledgment
-            handle_delete(new_socket);
-        } else {
-            printf("Invalid command received.\n");
+
+
+
+        while (1) {
+
+            // Receive command
+
+            memset(buffer, 0, BUF_SIZE);
+
+            int read_size = read(new_socket, buffer, BUF_SIZE);
+
+            if (read_size <= 0) {
+
+                printf("Connection closed or error occurred.\n");
+
+                break; // Exit command loop if connection closed or error
+
+            }
+
+
+
+            printf("Received command: %s\n", buffer);
+
+
+
+            if (strncmp(buffer, "UPLOAD", 6) == 0) {
+
+                send(new_socket, "ACK", 3, 0);  // Send acknowledgment
+
+                handle_upload(new_socket);
+
+            } else if (strncmp(buffer, "DOWNLOAD", 8) == 0) {
+
+                send(new_socket, "ACK", 3, 0);  // Send acknowledgment
+
+                handle_download(new_socket);
+
+            } else if (strncmp(buffer, "LIST", 4) == 0) {
+
+                handle_list(new_socket);
+
+            } else if (strncmp(buffer, "DELETE", 6) == 0) {
+
+                send(new_socket, "ACK", 3, 0);  // Send acknowledgment
+
+                handle_delete(new_socket);
+
+            } else {
+
+                printf("Invalid command received.\n");
+
+            }
+
         }
- 
+
+
+
         close(new_socket);
+
     }
- 
+
+
+
     close(server_fd);
+
     return 0;
+
 }
- 
+
+
+
 void handle_upload(int new_socket) {
+
     char buffer[BUF_SIZE] = {0};
+
     char filename[BUF_SIZE] = {0};
+
     int bytes_read, file_fd;
- 
+
+
+
     // Receive filename
+
     read(new_socket, filename, BUF_SIZE);
-    file_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
- 
+
+    // Construct full path within the root directory
+
+    char filepath[BUF_SIZE];
+
+    snprintf(filepath, BUF_SIZE, "%s/%s", ROOT_DIR, filename);
+
+
+
+    file_fd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+
     if (file_fd < 0) {
+
         perror("File open failed");
+
         return;
+
     }
- 
+
+
+
     // Receive file data
+
     while ((bytes_read = read(new_socket, buffer, BUF_SIZE)) > 0) {
+
         if (strncmp(buffer, END_OF_TRANSFER, strlen(END_OF_TRANSFER)) == 0) {
+
             break;
+
         }
+
         write(file_fd, buffer, bytes_read);
+
     }
- 
+
+
+
     close(file_fd);
-    printf("File %s uploaded successfully.\n", filename);
+
+    printf("File %s uploaded successfully.\n", filepath);
+
+
+
+    // Open the file
+
+    char command[BUF_SIZE];
+
+    snprintf(command, BUF_SIZE, "xdg-open %s", filepath); // Use `xdg-open` for Linux
+
+    system(command);
+
 }
- 
+
+
+
+
+
 void handle_download(int new_socket) {
+
     char buffer[BUF_SIZE] = {0};
+
     char filename[BUF_SIZE] = {0};
+
     int bytes_read, file_fd;
- 
+
+
+
     // Receive filename
+
     read(new_socket, filename, BUF_SIZE);
-    file_fd = open(filename, O_RDONLY);
- 
+
+    // Construct full path within the root directory
+
+    char filepath[BUF_SIZE];
+
+    snprintf(filepath, BUF_SIZE, "%s/%s", ROOT_DIR, filename);
+
+
+
+    file_fd = open(filepath, O_RDONLY);
+
     if (file_fd < 0) {
+
         perror("File open failed");
+
         return;
+
     }
- 
+
+
+
     // Send file data
+
     while ((bytes_read = read(file_fd, buffer, BUF_SIZE)) > 0) {
+
         send(new_socket, buffer, bytes_read, 0);
+
     }
- 
+
+
+
     // Send end of transfer signal
+
     send(new_socket, END_OF_TRANSFER, strlen(END_OF_TRANSFER), 0);
- 
+
+
+
     close(file_fd);
-    printf("File %s downloaded successfully.\n", filename);
+
+    printf("File %s downloaded successfully.\n", filepath);
+
 }
- 
+
+
+
 void handle_list(int new_socket) {
+
     struct dirent *de;
-    DIR *dr = opendir(".");
+
     char buffer[BUF_SIZE] = {0};
- 
+
+
+
+    DIR *dr = opendir(".");  // Open current working directory
+
     if (dr == NULL) {
+
         perror("Could not open directory");
+
         return;
+
     }
- 
+
+
+
     while ((de = readdir(dr)) != NULL) {
+
         strcat(buffer, de->d_name);
+
         strcat(buffer, "\n");
+
     }
- 
+
+
+
     write(new_socket, buffer, strlen(buffer));
+
     closedir(dr);
+
     printf("Directory listing sent.\n");
+
 }
- 
+
+
+
+
+
 void handle_delete(int new_socket) {
+
     char filename[BUF_SIZE] = {0};
- 
+
+
+
     // Receive filename
+
     read(new_socket, filename, BUF_SIZE);
- 
-    if (remove(filename) == 0) {
-        printf("File %s deleted successfully.\n", filename);
+
+    // Construct full path within the root directory
+
+    char filepath[BUF_SIZE];
+
+    snprintf(filepath, BUF_SIZE, "%s/%s", ROOT_DIR, filename);
+
+
+
+    if (remove(filepath) == 0) {
+
+        printf("File %s deleted successfully.\n", filepath);
+
     } else {
+
         perror("File delete failed");
+
     }
+
 }
- 
+
+
+
+
+
